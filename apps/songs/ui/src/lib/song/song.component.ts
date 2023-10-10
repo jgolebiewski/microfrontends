399ba3944b7b87257c8781eb5cca66fe434e1ba5
Janusz Gołębiewski <janusz@golebiewski.info.pl>
2023-10-10 19:04:24 +0200

    added tailwind

--- apps/songs/ui/src/lib/song/song.component.ts
+++ apps/songs/ui/src/lib/song/song.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 import { Song } from '@microfrontends/songs/model';
 
 @Component({
   selector: 'playlist-song',
   templateUrl: './song.component.html',
   styleUrls: ['./song.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SongComponent {
 
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          ...colors.indigo,
+          DEFAULT: colors.indigo[600],
+        },
+        accent: {
+          ...colors.slate,
+          DEFAULT: colors.slate[800],
+        },
+        warn: {
+          ...colors.red,
+          DEFAULT: colors.red[600],
+        },
+        'on-warn': {
+          500: colors.red['50'],
+        },
+        secondary: {
+          light: colors.green[600]
+        }
+      },
+    },
+    plugins: {},
+  },
+};
